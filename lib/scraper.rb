df23e77c066df3737481c948ebb66f01e6e61fec
kelly r maass <kmaass77627@gmail.com>
2019-12-27 20:07:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,9 +18,10 @@ class Scraper
   end
   
   def get_page 
-    
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
   end 
+  binding.pry 
   
 end
 
